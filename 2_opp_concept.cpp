131939a355d5a21d91d5645c3f2a606b3daf4f89
bhaveshdudhat22 <bhaveshdudhat@gmail.com>
2023-04-18 09:46:49 +0530

    Module-4
    Module-4

---
+++ 2_opp_concept.cpp
@@ -0,0 +1,26 @@
+/*
+What is OOP? List OOP concepts
+
+OOP stands for Object-Oriented Programming.
+
+C++ is an OOP language.
+
+Object-oriented programming is about creating objects that contain both data and functions.
+OOP is faster and easier to execute.
+OOP provides a clear structure for the programs.
+
+C++ was developed by Bjarne Stroustrup, as an extension to the C language.
+
+Extention:
+.c++ Or
+.cpp  (cpp stands for C Plus Plus)
+
+OOP Concept (features of OOP)
+1. Class/Object
+2. Encapsulation
+3. Inheritance
+4. Polymorphism
+5. Abstraction
+6. Data Binding or Dynamic Binding
+
+*/
